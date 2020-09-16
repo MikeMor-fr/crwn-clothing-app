fdfcaf718212305789e1c6bc3a540f2828a84e33
MikeMor-fr <62838338+MikeMor-fr@users.noreply.github.com>
2020-09-16 14:31:14 +0200

    added shop page component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import React from "react";
 import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
 import { Switch, Route } from "react-router-dom";
 
 import "./styles.css";
-
-const Hats = () => <h1>HATS PAGE</h1>;
 
 export default function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route path="/hats" component={Hats} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
