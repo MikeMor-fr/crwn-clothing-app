f5f60fdfa7195c5879bb9d6d0b7e9ef31344b302
MikeMor-fr <62838338+MikeMor-fr@users.noreply.github.com>
2020-09-17 17:40:45 +0200

    adding sign in component with custom button

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
+import SignPage from "./pages/sign/sign.component";
+
 import { Switch, Route } from "react-router-dom";
 
 import "./styles.css";
@@ -13,6 +15,7 @@ export default function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignPage} />
       </Switch>
     </div>
   );
