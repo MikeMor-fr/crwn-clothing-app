483ed83a6b5b6b7e820fc7ad8790c4047a1ce435
MikeMor-fr <62838338+MikeMor-fr@users.noreply.github.com>
2020-09-21 20:54:26 +0200

    adding firebase

---
+++ src/firebase/firebase.util.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase/app';
+import 'firebade/firestore';
+import 'firebase/auth';
+
+const config = {
+  
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GithubAuthProvider();
