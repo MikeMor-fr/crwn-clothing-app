e828c20020d74886b56c858038977fa232311b84
MikeMor-fr <62838338+MikeMor-fr@users.noreply.github.com>
2020-09-22 14:55:39 +0200

    added subscribed authentification in application

--- src/App.js
+++ src/App.js
@@ -1,22 +1,48 @@
-import React from "react";
+import React, { Component } from "react";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
 import SignPage from "./pages/sign/sign.component";
+import { auth } from "./firebase/firebase.util";
 
 import { Switch, Route } from "react-router-dom";
 
 import "./styles.css";
 
-export default function App() {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/shop" component={ShopPage} />
-        <Route path="/signin" component={SignPage} />
-      </Switch>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      currentUser: null
+    };
+  }
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
+      this.setState({ currentUser: user });
+
+      console.log(user);
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/shop" component={ShopPage} />
+          <Route path="/signin" component={SignPage} />
+        </Switch>
+      </div>
+    );
+  }
 }
+
+export default App;
